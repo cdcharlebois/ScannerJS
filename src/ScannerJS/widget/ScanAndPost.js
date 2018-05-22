c57f3eefa47d43211427426dc72a847d71251176
Conner Charlebois <cdcharlebois@gmail.com>
2018-05-22 11:36:53 -0400

    init

---
+++ src/ScannerJS/widget/ScanAndPost.js
@@ -0,0 +1,19 @@
+import {
+    defineWidget,
+    log,
+    runCallback,
+} from 'widget-base-helpers';
+
+export default defineWidget('ScanAndPost', false, {
+
+    _obj: null,
+
+    constructor() {
+        this.log = log.bind(this);
+        this.runCallback = runCallback.bind(this);
+    },
+
+    postCreate() {
+        log.call(this, 'postCreate', this._WIDGET_VERSION);
+    },
+});
